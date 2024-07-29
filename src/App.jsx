01d2b8393e3126bcb2d3e083a5aac50bc8ce39a3
kelvin <kelvinmutugi336@gmil.com>
2024-07-29 14:53:29 +0300

    adds a navbar and home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <>phase 5 project</>;
+  return (
+    <>
+      phase 5 project
+      <Home />
+    </>
+  );
 }
 
 export default App;
