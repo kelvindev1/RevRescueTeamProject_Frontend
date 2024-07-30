3b50328be19f538acdd0949dccbc74b8a970c0b3
kelvin <kelvinmutugi336@gmil.com>
2024-07-30 07:31:29 +0300

    updates fotter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
-// import Footer from "./Footer";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <div>
       <Navbar />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
